25353df4010eda31cc1832e2dc6b45321b350373
tom <mail2tomer@gmail.com>
2021-12-21 18:22:28 +0200

    updated static root dep

--- JewelryProject/settings.py
+++ JewelryProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from .local_settings import SECRET_KEY, DATABASES, BASE_DIR
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -109,11 +111,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
